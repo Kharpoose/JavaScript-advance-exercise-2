94aa9bfd7dc3e75146b86feffb9b787dc7203eab
Çağrı <karpuzcagri@gmail.com>
2024-02-20 18:24:05 +0300

    Update main.js

--- JavaScript advance function exercise 2/main.js
+++ JavaScript advance function exercise 2/main.js
@@ -13,3 +13,19 @@ const multiplyshort = (a, b) => a * b;
 console.log(multiplyshort(8, 7));
 console.log(multiplyshort(2, 5));
 console.log(Math.round( multiplyshort(32, 17) / 22));
+
+
+function countPositive(nums) {
+    let positiveNumbers = 0;
+
+    nums.forEach((num) => {
+      if (num > 0) {
+        positiveNumbers++;
+      }
+    });
+
+    return positiveNumbers;
+  }
+
+  console.log(countPositive([1, -3, -5]));
+  console.log(countPositive([-2, -3, -5, 7, 10]));
