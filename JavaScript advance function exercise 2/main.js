f4a7b8e2586bf4bdea8cbacdd76aaaa2f0811250
Çağrı <karpuzcagri@gmail.com>
2024-02-20 18:17:46 +0300

    Update main.js

--- JavaScript advance function exercise 2/main.js
+++ JavaScript advance function exercise 2/main.js
@@ -1,4 +1,15 @@
-console.log("Test 1");
-console.log("Test 2");
-console.log("Test 3");
-console.log("Test again Not working :(")
+const multiply = (a, b) => {
+    return a * b;
+};
+
+console.log(multiply(3, 5));
+console.log(multiply(8, 5));
+console.log(Math.round( multiply(32, 15) / 22));
+
+console.log("")
+
+const multiplyshort = (a, b) => a * b;
+
+console.log(multiplyshort(8, 7));
+console.log(multiplyshort(2, 5));
+console.log(Math.round( multiplyshort(32, 17) / 22));
